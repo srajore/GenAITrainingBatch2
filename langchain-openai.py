76c600b9372024d702a998c0136b84814aed2c57
Sharad <sharad.rajore@zensar.com>
2024-11-11 10:31:31 +0530

    removed api key from code

--- langchain-openai.py
+++ langchain-openai.py
@@ -4,7 +4,7 @@
 from langchain_openai import OpenAI
 
 
-llm=OpenAI(api_key="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",model_name="gpt-3.5-turbo-instruct")
+llm=OpenAI(api_key="",model_name="gpt-3.5-turbo-instruct")
 
 prompt="What is Generative AI ?"
 
