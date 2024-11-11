c05e52c573081363e92be2ced97ef70b062485b3
Sharad <sharad.rajore@zensar.com>
2024-11-11 10:42:14 +0530

    modified

--- langchain-openai.py
+++ langchain-openai.py
@@ -4,7 +4,7 @@
 from langchain_openai import OpenAI
 
 
-llm=OpenAI(api_key="",model_name="gpt-3.5-turbo-instruct")
+llm=OpenAI(api_key="",model_name="gpt-3.5-turbo-instruct") 
 
 prompt="What is Generative AI ?"
 
